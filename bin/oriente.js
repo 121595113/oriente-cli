2232113cfdaf243ffd805052799d97612ee3bf45
TaoYu <121595113@qq.com>
2018-05-31 18:25:14 +0800

    新增添加插件功能

--- bin/oriente.js
+++ bin/oriente.js
@@ -16,6 +16,9 @@ if (process.argv.length <= 2) {
 program
   .version(require('../package').version)
   .usage('<command> [options]')
+  .option('--namespace [value]', '设置插件命名空间', '')
+  .option('--config [boolean]', '是否更新配置文件', 'true')
+  .option('--type [value]', '命名空间创建方式', 'clobbers')
 
 program
   .command('create <name> [path]')
@@ -46,4 +49,17 @@ program
     console.log('    $ oriente remove module2,module2');
     console.log();
   });
+
+program
+  .command('plugin <method> <name>')
+  .description('插件相关操作')
+  .action((method, name) => {
+    if(method === 'add') {
+      utils.pluginAdd(name, program.namespace, program.type, program.config === 'true')
+    }
+    if(method === 'remove') {
+      utils.pluginRemove(name, program.config !== 'true')
+    }
+  });
+
 program.parse(process.argv);
---
+++ bin/plugin.js
@@ -0,0 +1,111 @@
+function lastName(name) {
+  return name.substr(name.lastIndexOf('-') + 1 || 0)
+}
+
+exports.lastName = lastName
+
+exports.setItem = function (name, nameSpace, type) {
+  return {
+    "type": "ObjectExpression",
+    "start": 98,
+    "end": 312,
+    "properties": [{
+        "type": "Property",
+        "start": 108,
+        "end": 144,
+        "method": false,
+        "shorthand": false,
+        "computed": false,
+        "key": {
+          "type": "Literal",
+          "start": 108,
+          "end": 112,
+          "value": "id",
+          "raw": "\"id\""
+        },
+        "value": {
+          "type": "Literal",
+          "start": 114,
+          // "end": 144,
+          "value": `cordova-plugin-${name}.${lastName(name)}`,
+          "raw": `"cordova-plugin-${name}.${lastName(name)}"`
+        },
+        "kind": "init"
+      },
+      {
+        "type": "Property",
+        "start": 154,
+        "end": 207,
+        "method": false,
+        "shorthand": false,
+        "computed": false,
+        "key": {
+          "type": "Literal",
+          "start": 154,
+          "end": 160,
+          "value": "file",
+          "raw": "\"file\""
+        },
+        "value": {
+          "type": "Literal",
+          "start": 162,
+          "end": 207,
+          "value": `plugins/cordova-plugin-${name}/www/${name}.js`,
+          "raw": `"plugins/cordova-plugin-${name}/www/${name}.js"`
+        },
+        "kind": "init"
+      },
+      {
+        "type": "Property",
+        "start": 217,
+        "end": 252,
+        "method": false,
+        "shorthand": false,
+        "computed": false,
+        "key": {
+          "type": "Literal",
+          "start": 217,
+          "end": 227,
+          "value": "pluginId",
+          "raw": "\"pluginId\""
+        },
+        "value": {
+          "type": "Literal",
+          "start": 229,
+          // "end": 252,
+          "value": `cordova-plugin-${name}`,
+          "raw": `"cordova-plugin-${name}"`
+        },
+        "kind": "init"
+      },
+      {
+        "type": "Property",
+        "start": 262,
+        "end": 306,
+        "method": false,
+        "shorthand": false,
+        "computed": false,
+        "key": {
+          "type": "Literal",
+          "start": 262,
+          "end": 272,
+          "value": type || "clobbers",
+          "raw": `${type || "clobbers"}`
+        },
+        "value": {
+          "type": "ArrayExpression",
+          "start": type ? 264 + type.length : 274,
+          // "end": 306,
+          "elements": [{
+            "type": "Literal",
+            "start": 288,
+            // "end": 296,
+            "value": `${nameSpace ? nameSpace : 'cordova.plugins.' + lastName(name)}`,
+            "raw": `"${nameSpace ? nameSpace : 'cordova.plugins.' + lastName(name)}"`
+          }]
+        },
+        "kind": "init"
+      }
+    ]
+  }
+}
